b8867bbe9ecdf18836f563b677194c9c15e5df95
Foco <87334856+YTFoco@users.noreply.github.com>
2026-02-24 19:31:45 +0000

    Add configuration for notification position

---
+++ config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+--[[
+    NOTIFICATION POSITION
+    ---------------------
+    Choose one of the following options:
+
+    "top-left"       "top-center"       "top-right"
+    "middle-left"    "middle-center"    "middle-right"
+    "bottom-left"    "bottom-center"    "bottom-right"
+]]
+Config.Position = "top-center"
